249c1328ac90ea536fa9be437c537643f79867a9
Saxdel <andreihincu1999@gmail.com>
2022-04-06 18:05:09 +0300

    Added Station CRUD.

--- TPL/Controllers/StationController.cs
+++ TPL/Controllers/StationController.cs
@@ -19,16 +19,37 @@ namespace TPL.Controllers
             this.stationService = stationService;
         }
 
-        
+
         [JwtAuthorizeAttribute]
         [HttpPost("Create")]
         public async Task<IActionResult> CreateStation(StationCreateDto stationDto, string token)
         {
-
             var response = await stationService.CreateStation(stationDto, token);
             return Ok(response);
+        }
 
+        [JwtAuthorizeAttribute]
+        [HttpGet("Get-All")]
+        public async Task<IActionResult> GetAllStations(string token)
+        {
+            var response = await stationService.GetAllStations(token);
+            return Ok(response);
+        }
 
+        [JwtAuthorizeAttribute]
+        [HttpDelete("DeleteById")]
+        public async Task<IActionResult> DeleteStation(Guid id, string token)
+        {
+            await stationService.DeleteStations(id, token);
+            return Ok();
+        }
+        [JwtAuthorizeAttribute]
+        [HttpPut("Update")]
+        public async Task<IActionResult> UpdateStation(StationUpdateDto dto, string token)
+        {
+
+            var response = await stationService.UpdateStation(dto, token);
+            return Ok(response);
         }
     }
 }
--- TPL/Services/Interfaces/IStationService.cs
+++ TPL/Services/Interfaces/IStationService.cs
@@ -9,5 +9,9 @@ namespace TPL.Services.Interfaces
     public interface IStationService
     {
         Task<StationResponseDto> CreateStation(StationCreateDto stationDto, string token);
+        Task<List<StationResponseDto>> GetAllStations(string token);
+        Task DeleteStations(Guid id, string token);
+        Task<StationResponseDto> UpdateStation(StationUpdateDto dto, string token);
+
     }
 }
